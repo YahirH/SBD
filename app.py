98988e8685154c680001dd59b00f1f0040d1dd15
l4r05 <angel.reyesolvr@uanl.edu.mx>
2022-05-05 20:26:26 -0500

    Se agrego la parte de crear alumno

--- app.py
+++ app.py
@@ -1,6 +1,108 @@
+# External modules
+from colorama import init, Fore
+import time
+import os
+# Local modules
 import crud
 
-# Inicializar Crud
-app = crud.db()
-app.CrearAlumno(2,1,1,"Prueba2",21,"H")
-app.close_connection()
+def clear():
+    os.system("cls")
+
+
+init(autoreset= True)
+
+def validation(msg, a, b):
+    while True:
+        clear()
+        try:
+            logo()
+            print("\n" + msg + "\n")
+            opc = int(input("Ingrese una opcion: "))
+            if opc >= a and opc <= b:
+                return opc
+            else:
+                clear()
+                print(Fore.RED + "[ X ] Oops!, Opcion invalida... Intente de nuevo.")
+                time.sleep(2)
+                clear()
+
+        except ValueError:
+            clear()
+            print(Fore.RED + "[ X ] Oops!, Opcion invalida... Intente de nuevo.")
+            time.sleep(2)
+            clear()
+
+def logo():
+    print("\t\t" + "#"*65, end="\n\t\t#" + " "*63 + "#\n")
+    print("\t\t#\t" + Fore.RED             + " /$$   /$$  /$$$$$$  /$$   /$$  /$$$$$$  /$$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.BLUE            + "| $$  | $$ /$$__  $$| $$$ | $$ /$$__  $$| $$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.YELLOW          + "| $$  | $$| $$  \ $$| $$$$| $$| $$  \ $$| $$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.MAGENTA         + "| $$  | $$| $$$$$$$$| $$ $$ $$| $$$$$$$$| $$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.CYAN            + "| $$  | $$| $$__  $$| $$  $$$$| $$__  $$| $$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.GREEN           + "| $$  | $$| $$  | $$| $$\  $$$| $$  | $$| $$       " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.WHITE           + "|  $$$$$$/| $$  | $$| $$ \  $$| $$  | $$| $$$$$$$$ " + Fore.RESET +"\t#")
+    print("\t\t#\t" + Fore.LIGHTMAGENTA_EX + " \______/ |__/  |__/|__/  \__/|__/  |__/|________/ " + Fore.RESET +"\t#", end="\n\t\t#" + " "*63 + "#\n")
+    print("\t\t" + "#"*65)
+
+
+def menu():
+    msg = "[ 1 ] Crear\n[ 2 ] Mostrar\n[ 3 ] Modificar\n[ 4 ] Eliminar\n[ 5 ] Salir"
+
+    opc = validation(msg, 1, 5)
+
+    if opc == 1:
+        crear()
+    elif opc == 2:
+        mostrar()
+    elif opc == 3:
+        modificar()
+    elif opc == 4:
+        eliminar()
+    else:
+        exit()
+
+    return opc
+
+
+def crear():
+    msg = "[ 1 ] Registrar Trabajador\n[ 2 ] Registrar Alumno\n[ 3 ] Agregar Materias\n[ 4 ] Regresar"
+    opc = validation(msg, 1, 4)
+
+    clear()
+    if opc == 1:
+        app.CrearTrabajador()
+    elif opc == 2:
+        app.CrearAlumno()
+    elif opc == 3:
+        app.CrearMaterias()
+
+    return opc
+
+
+def mostrar():
+    msg = "[ 1 ] Mostrar Alumnos\n[ 2 ] Mostrar todos los trabajadores\n[ 3 ] Mostrar Profesores\n[ 4 ] Mostrar Facultades\n[ 5 ] Mostrar Todas las materias\n[ 6 ] Regresar"
+    opc = validation(msg, 1, 6)
+
+    return opc
+
+def modificar():
+    msg = "[ 1 ] Modificar datos de alumno\n[ 2 ] Modificar datos de trabajador\n[ 3 ] Modificar Materia\n[ 4 ] Regresar"
+    opc = validation(msg, 1, 4)
+
+    return opc
+
+def eliminar():
+    msg = "[ 1 ] Eliminar un alumno\n[ 2 ] Eliminar trabajador\n[ 3 ] Eliminar Materia\n[ 4 ] Regresar"
+    opc = validation(msg, 1, 4)
+
+    return opc
+
+
+if __name__ == '__main__':
+
+    # Inicializar Crud
+    app = crud.db()
+
+    menu()
+
+    app.close_connection()
--- crud.py
+++ crud.py
@@ -1,8 +1,26 @@
+from tkinter import N
 from colorama import Fore
 import pyodbc
-                     
-
 class db():
+
+    # Id Facultades
+    facultades = {
+        1: {'nombre': 'FCFM', 'id': 1, 'carrera': {1: 'LSTI', 2: 'LCC', 3: 'LA'}},
+        2: {'nombre': 'FCQ', 'id': 2, 'carrera': {4: 'LQFB', 5: 'IIA'}},
+        3: {'nombre': 'FIME', 'id': 3, 'carrera': {6: 'ITS', 7: 'IAS', 8: 'IA'}},
+        4: {'nombre': 'FCC', 'id': 4, 'carrera': {9: 'LCC', 10: 'LMGI'}},
+        5: {'nombre': 'FAV', 'id': 5, 'carrera': {11: 'LDG'}},
+        6: {'nombre': 'FACMED', 'id': 6, 'carrera': {12: 'MED'}},
+    }
+
+    departamentos = {
+        'Direccion': 1,
+        'Tesoreria': 2,
+        'Prefectura': 3,
+        'Servicios Generales': 4
+    }
+
+
     def __init__(self, driver="{ODBC Driver 17 for SQL SERVER}", server="LENOVO\SQLEXPRESS", database="UANAL", username="Python", pwd="1234"):
         self.driver = driver
         self.server = server
@@ -34,12 +52,52 @@ class db():
 
             self.cnxn.commit()
         except:
-            print("Error al insertar los datos")
+            print(Fore.RED + "Hubo un error al realizar la consulta a la base de datos.")
 
-    def CrearAlumno(self, Id, idFacultad, idCarrera, nombre, edad, sexo):
-        query = f"INSERT INTO Alumno(Id,IdFacultad,IdCarrera,Nombre,Edad,Sexo) VALUES ('{Id}','{idFacultad}','{idCarrera}','{nombre}','{edad}','{sexo}');"
+
+    def CrearAlumno(self):
+        
+        nombre = str(input("Nombre del alumno: "))
+        edad = int(input("\nEdad del Alumno: "))
+        sexo = str(input("\nSexo del alumno: "))
+
+        print("\n============= SELECCIONE LA FACULTAD =================")
+        count = 1
+        for facultades in self.facultades.values():
+            print(f"[ {count} ] {facultades['nombre']}")
+            count += 1
+        idFacultad = int(input("\nSeleccione la facultad: "))
+
+        print("\n============== SELECCIONE LA CARRERA =================")
+        count = 1
+        for carrera in self.facultades[idFacultad]['carrera'].values():
+            print(f"[ {count} ] {carrera}")
+            count += 1
+        idCarrera = int(input("\nSeleccione la carrera: "))
+
+        query = f"INSERT INTO Alumno(IdFacultad,IdCarrera,Nombre,Edad,Sexo) VALUES ('{idFacultad}','{idCarrera}','{nombre}','{edad}','{sexo}');"
         db.execute_query(self, query)
 
-    def CrearTrabajador(self, Id, idDepartamento, nombre, apellidos, edad, curp, sexo, area):
-        query = f"INSERT INTO Trabajador(Id,IdDepartamento,Nombre,Apellidos,Edad,CURP,sexo,area) VALUES ('{Id}','{idDepartamento}','{nombre}','{apellidos}','{edad}','{curp}', '{sexo}', '{area}');"
+        print(Fore.GREEN + f"\nEl alumno {nombre} se a registrado con exito!\n")
+
+
+    def CrearTrabajador(self):
+        Id = int(input("Id del Trabajador: "))
+        idDepartamento = int(input("Id del departamento: "))
+        nombre = str(input("Nombre del Trabajador: "))
+        apellidos = int(input("Apellidos del Trabajador: "))
+        edad = int(input("Edad del Trabajador: "))
+        sexo = str(input("Sexo del trabajador: "))
+        area = str(input("Area del trabajador: "))
+
+        query = f"INSERT INTO Trabajador(Id,IdDepartamento,Nombre,Apellidos,Edad,sexo,area) VALUES ('{Id}','{idDepartamento}','{nombre}','{apellidos}','{edad}', '{sexo}', '{area}');"
+        db.execute_query(self, query)
+
+
+    def CrearMaterias(self):
+        Id = int(input("Id de la materia: "))
+        idSemestre = int(input("Id del semestre: "))
+        nombre = str(input("Nombre de la materia: "))
+
+        query = f"INSERT INTO Asignatura(Id,IdSemestre,Nombre) VALUES ('{Id}','{idSemestre}','{nombre}')"
         db.execute_query(self, query)
